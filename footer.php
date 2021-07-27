728e18f51eed32f90598077ea80a9fd09a7b6663
Phan Kim Ngân <msngan.website@gmail.com>
2021-07-27 23:00:40 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,20 +5,22 @@
         ?>
     </div>
     <div id="copyright">
-        <p>
-        	<a href="https://app.phankimngan.com/"><b>APP.PHANKIMNGAN.COM</b></a>
-        </p>
-        <p>
-        	<?php if($version!="")echo "Phiên bản: $version – "; ?> Copyright &copy; 2015 - <?php echo date( 'Y' ); ?> by <a href="https://info.phankimngan.com/" title="Phan Kim Ngân" target="_blank">Phan Kim Ngân</a>
-        </p>
-        <p>
-        	<a href="https://zalo.me/chincucarot" target="_blank">Zalo Kim Ngân</a> – 
-            <a href="https://www.facebook.com/msngan.beauty/" target="_blank">Facebook Kim Ngân</a> – 
-            <a href="mailto:msngan.website@gmail.com" target="_blank">Email Dev Support</a> – 
-            <a href="https://info.phankimngan.com" target="_blank">My Info</a> – 
-            <a href="https://github.com/msnganwebsite/" target="_blank">Github.com</a>
-        </p>
-        <p>Đây là nội dung mẫu demo. Bạn có thể chỉnh sửa các nội dung tại chân trang này để phù hợp với mong muốn.</p>
+        <div class="copyright">
+            <p>
+                <a href="https://app.phankimngan.com/"><b>APP.PHANKIMNGAN.COM</b></a>
+            </p>
+            <p>
+                <?php if($version!="")echo "Phiên bản: $version – "; ?> Copyright &copy; 2015 - <?php echo date( 'Y' ); ?> by <a href="https://info.phankimngan.com/" title="Phan Kim Ngân" target="_blank">Phan Kim Ngân</a>
+            </p>
+            <p>
+                <a href="https://zalo.me/chincucarot" target="_blank">Zalo Kim Ngân</a> – 
+                <a href="https://www.facebook.com/msngan.beauty/" target="_blank">Facebook Kim Ngân</a> – 
+                <a href="mailto:msngan.website@gmail.com" target="_blank">Email Dev Support</a> – 
+                <a href="https://info.phankimngan.com" target="_blank">My Info</a> – 
+                <a href="https://github.com/msnganwebsite/" target="_blank">Github.com</a>
+            </p>
+            <p>Đây là nội dung mẫu demo. Bạn có thể chỉnh sửa các nội dung tại chân trang này để phù hợp với mong muốn.</p>
+        </div>
     </div>
     <?php echo $insert_foot; ?>
 </body>
